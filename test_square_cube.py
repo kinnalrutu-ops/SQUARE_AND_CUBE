2edb87426379859442650b3021cf7f15dddfb885
Rutu <kinnalrutu@gmail.com>
2026-01-16 12:10:03 +0530

    Update test_square_cube.py

--- test_square_cube.py
+++ test_square_cube.py
@@ -1,4 +1,7 @@
 from sq_cube import square_cube
+
 def test_square_cube():
-    assert square == [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
-    assert cube == [1, 8, 27, 64, 125, 216, 343, 512, 729, 1000]
+    squares, cubes = square_cube()
+
+    assert squares == [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+    assert cubes == [1, 8, 27, 64, 125, 216, 343, 512, 729, 1000]
