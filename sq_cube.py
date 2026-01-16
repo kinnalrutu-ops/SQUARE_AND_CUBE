ebcc2c3d1581ae17bbd0842e7fbb47aaa84de3f0
Rutu <kinnalrutu@gmail.com>
2026-01-16 11:54:10 +0530

    Update sq_cube.py

--- sq_cube.py
+++ sq_cube.py
@@ -1,12 +1,15 @@
 def square_cube(n):
-    arr=[]
+    arr1=[]
+    arr2=[]
     square = n * n
     cube = n * n * n
-    arr.append(square)
-    arr.append(cube)
+    arr1.append(square)
+    arr2.append(cube)
     return square, cube
 
 if __name__ == "__main__":
     s, c = square_cube(10)
+    print(arr1)
+    print(arr2)
     print("Square:", s)
     print("Cube:", c)
